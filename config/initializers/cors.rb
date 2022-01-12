6b6c56ac7d979852e632fbc69aeaa291e699f5b6
Alexandre Sampaio <thndrmaster@gmail.com>
2022-01-12 13:28:58 -0300

    Configurando o CORS da Aplicação

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -14,3 +14,16 @@
 #       methods: [:get, :post, :put, :patch, :delete, :options, :head]
 #   end
 # end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins '*'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+end
+
+unless Rails.env.test?
+  Rails.application.config.middleware.use AppName, "DESAFIO-NINJA [API] - #OK!!!"
+end
